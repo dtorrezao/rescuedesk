3b538eaac557c049951dc38217383de02083a8ed
dtorrezao <wazza.torrezao@gmail.com>
2018-02-11 15:16:53 +0000

    Controlador de Pedidos Adicionado

---
+++ app/RescueDesk/Controllers/PedidosController.cs
@@ -0,0 +1,25 @@
+﻿using RescueDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RescueDesk.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Index()
+        {
+            List<Pedido> pedidos = new List<Pedido>();
+            Pedido xpto = new Pedido();
+
+            xpto.id = 1;
+
+            pedidos.Add(xpto);
+
+            return View(pedidos);
+        }
+    }
+}
---
+++ app/RescueDesk/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RescueDesk.Models
+{
+    public class Pedido
+    {
+        public string title { get; set; }
+
+        public int id { get; set; }
+    }
+}
