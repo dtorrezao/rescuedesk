a82f5d20f8f7fe4b9f2406e61e40d48bbd26c358
David Torrezão <wazza.torrezao@gmail.com>
2018-04-15 12:56:03 +0100

    Listar localidades adicionado

---
+++ app/RescueDesk/Controllers/LocalidadesController.cs
@@ -0,0 +1,20 @@
+﻿using RescueDesk.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RescueDesk.Controllers
+{
+    public class LocalidadesController : Controller
+    {
+        // GET: Localidades
+        public ActionResult Index()
+        {
+            AddressService addressService = new AddressService();
+                
+            return View(addressService.ObterLocalidades());
+        }
+    }
+}
