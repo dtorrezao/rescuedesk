61e3e47fa7fe9ad1d4c5d7ec90053d03f3714060
Unknown <carlosfalhuscas@gmail.com>
2018-05-21 17:41:50 +0100

    Views departamentos adicionado

--- app/RescueDesk/Models/Departamento.cs
+++ app/RescueDesk/Models/Departamento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace RescueDesk.Models
 {
     public class Departamento
     {
+        [DisplayName("ID")]
         public int iddept { get; set; }
+        [DisplayName("Departamento")]
         public string dept { get; set; }
     }
 }
